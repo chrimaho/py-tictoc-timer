60896cf20567118cf7c73d62d110b4c5a84bf255
chrimaho <chrismahoney@hotmail.com>
2022-09-07 18:57:20 +1000

    Remove unnecessary files

--- src/cli/lint/black.sh
+++
@@ -1,1 +0,0 @@
-python -m pipenv run python -m black --safe py_tictoc_timer tests
--- src/cli/test/test.sh
+++
@@ -1,5 +0,0 @@
-python -m pipenv run python -m pytest --verbose --cov=py_tictoc_timer --cov-report=term --cov-report=html:cov-report/html --cov-report=xml:cov-report/xml/cov-report.xml
-
-pipenv run pytest --verbose --cov=py_tictoc_timer --cov-report=term --cov-report=html:cov-report/html --cov-report=xml:cov-report/xml/cov-report.xml
-
-pipenv run mypy py_tictoc_timer --ignore-missing-imports --pretty --install-types --non-interactive
